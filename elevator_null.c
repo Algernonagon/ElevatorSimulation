28c41365fc22962d1dcf9c6e9cad4794d4aff20b
Nathan Yang <65429172+Algernonagon@users.noreply.github.com>
2020-11-21 13:02:04 -0500

    Update elevator_null.c
    Added comments to make code more understandable

--- elevator_null.c
+++ elevator_null.c
@@ -51,11 +51,13 @@ void wait_for_elevator(Person *p)
 	Dllist people_waiting = ((Sim_Global *)(e->es->v))->people_waiting;
 	pthread_cond_t *cond = ((Sim_Global *)(e->es->v))->cond;
 	
+	//Append person to wait list and signal elevators
 	pthread_mutex_lock(p->es->lock);
 	dll_append(people_waiting, new_jval_v(p));
 	pthread_cond_signal(cond);
 	pthread_mutex_unlock(p->es->lock);
 
+	//Block until assigned an elevator and given signal to get on
 	pthread_mutex_lock(p->lock);
 	while(p->e == NULL) {
 		pthread_cond_wait(p->cond, p->lock);
@@ -65,10 +67,12 @@ void wait_for_elevator(Person *p)
 
 void wait_to_get_off_elevator(Person *p)
 {
+	//Signal elevator to move to person's 'to' destination
 	pthread_mutex_lock(p->e->lock);
 	pthread_cond_signal(p->e->cond);
 	pthread_mutex_unlock(p->e->lock);
 
+	//Block until elevator at destination
 	pthread_mutex_lock(p->lock);
 	while(p->e->onfloor != p->to) {
 		pthread_cond_wait(p->cond, p->lock);
@@ -78,6 +82,7 @@ void wait_to_get_off_elevator(Person *p)
 
 void person_done(Person *p)
 {
+	//Signal elevator that person is done
 	pthread_mutex_lock(p->e->lock);
 	pthread_cond_signal(p->e->cond);
 	pthread_mutex_unlock(p->e->lock);
@@ -92,6 +97,7 @@ void *elevator(void *arg)
 		Dllist people_waiting = ((Sim_Global *)(e->es->v))->people_waiting;
 		pthread_cond_t *cond = ((Sim_Global *)(e->es->v))->cond;
 
+		//Block until there are people waiting and given signal, then take a person off the wait list
 		pthread_mutex_lock(e->es->lock);
 		while(dll_empty(people_waiting)) {
 			pthread_cond_wait(cond, e->es->lock);
@@ -102,6 +108,7 @@ void *elevator(void *arg)
 		//pthread_mutext_lock(p->lock);
 		pthread_mutex_unlock(e->es->lock);
 
+		//Move to person's floor, put self as person's elevator, and signal the person to get on
 		pthread_mutex_lock(p->lock);
 		move_to_floor(e, p->from);
 		open_door(e);
@@ -109,6 +116,7 @@ void *elevator(void *arg)
 		pthread_cond_signal(p->cond);
 		pthread_mutex_unlock(p->lock);
 		
+		//Block until person is on elevator and signaled to move, then take them to their destination
 		pthread_mutex_lock(e->lock);
 		//e->onfloor = p->from;
 		while(dll_empty(e->people)) {
@@ -119,10 +127,12 @@ void *elevator(void *arg)
 		move_to_floor(e, p->to);
 		open_door(e);
 
+		//Signal person to get off
 		pthread_mutex_lock(p->lock);
 		pthread_cond_signal(p->cond);
 		pthread_mutex_unlock(p->lock);
 
+		//Block until person gets off and signals they are done.
 		pthread_mutex_lock(e->lock);
 		//e->onfloor = p->to;
 		while(!dll_empty(e->people)) {
